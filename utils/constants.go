be0b4798c6abb79a406be76391e8c724b3515460
youlingdada <youlingdada@163.com>
2023-02-06 12:24:01 +0800

    服务器部署脚本

--- utils/constants.go
+++ utils/constants.go
@@ -1,8 +1,8 @@
 package utils
 
 const (
-	StaticRoot     = "./public/"
-	UploadFileRoot = "upload/%d/%d/%d/" // 年/月/日/
-	//DeployStaticRoot     = "http://43.136.170.73/"
-	DeployStaticRoot = "http://192.168.2.8:8080/public/"
+	StaticRoot       = "./public/"
+	UploadFileRoot   = "upload/%d/%d/%d/" // 年/月/日/
+	DeployStaticRoot = "http://43.138.127.168:8080/public/"
+	//DeployStaticRoot = "http://192.168.2.8:8080/public/"
 )
